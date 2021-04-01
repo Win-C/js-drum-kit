8a6648fad3357c00ee0080c0e4172638228fb0e0
Winnie Chou <winnie.chou.mb@gmail.com>
2021-04-01 17:42:02 -0500

    completed 1/30 for JavaScript30

---
+++ drumkit.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** Function to play audio based on key */
+function playSound(evt){
+  // keyCode is associated with every key on keyboard
+  const audio = document.querySelector(`audio[data-key="${evt.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${evt.keyCode}"]`); 
+  if(audio){
+    audio.currentTime = 0; // Rewind to the start to allow audio to play multiple times
+    audio.play()
+    key.classList.add('playing');
+  }
+}
+
+/** Function removes the 'playing' class when transition has ended */
+function removeTransition(evt){
+  if (evt.propertyName == 'transform'){
+    console.log(evt.propertyName);
+    this.classList.remove('playing');
+  }
+}
+
+// Get all keys and add an event listener for each key
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// Add event listener for keydown
+window.addEventListener('keydown', playSound);
